84b84ccdba67752466886228ca1460d8a3eec9d9
TaherSaid <amosiwar@gmail.com>
2022-06-17 12:49:23 +0200

    create add todoItem form

---
+++ src/components/AddTodoForm/index.tsx
@@ -0,0 +1,15 @@
+const AddTodoForm = () => {
+  //   const onSubmit = (event: object) => {
+  //     console.log(event.target.value);
+  //   };
+  return (
+    <div>
+      <form>
+        <input className="" name="todoItem" placeholder="TodoItem" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTodoForm;

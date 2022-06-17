d789e418cec35495a49d976f3e92c299f06a048f
TaherSaid <amosiwar@gmail.com>
2022-06-17 14:37:05 +0200

    add todo List item

--- src/components/TodoListItem/index.tsx
+++ src/components/TodoListItem/index.tsx
@@ -1,5 +1,20 @@
-const TodoListItem = () => {
-  return <div>TodoListItem</div>;
+const TodoListItem: React.FC<Props> = ({ todo, ToggleTodo }) => {
+  return (
+    <li>
+      <label
+        style={{ textDecoration: todo.complete ? "line-through" : undefined }}
+      >
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={() => {
+            ToggleTodo(todo);
+          }}
+        />
+        {todo.text}
+      </label>
+    </li>
+  );
 };
 
 export default TodoListItem;
